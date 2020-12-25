3d29f1747ff548f79d84f5191c1718968bda14d2
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-12-25 13:00:43 +0200

    Add files via upload

---
+++ MyCalculator/CalculatorTest/CalculatorNTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculator;
+using System;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorNTest
+    {
+        [TestMethod]
+        public void Calculator_Add_Std()
+        {
+            // Arrange
+            Calculator calc = new Calculator();
+            int a = 7;
+            int b = 3;
+            int expected_result = 10;
+
+            // Act
+            int result = calc.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected_result+1, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 0);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}
---
+++ MyCalculator/CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a10e6079-342f-4184-b25b-935f6d901618")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
